7f09add0e42a625172113516e95d6f3cfe985365
iberryful <iberryful@gmail.com>
2018-08-03 07:10:25 +0000

    fix Dockerfile

--- infrabox/entrypoint.sh
+++ infrabox/entrypoint.sh
@@ -13,6 +13,11 @@ if [ -z $TARGET_PROFILE ];then
 	exit 1
 fi
 
+echo "##Install ccache"
+git clone https://github.com/ccache/ccache.git /tmp/ccache && \
+	    pushd /tmp/ccache && git checkout v3.4.2 && ./autogen.sh && ./configure && \
+	        make && make install && popd && rm -rf /tmp/ccache
+
 #mkdir -p infrabox/cache/ccache
 #export CCACHE_DIR=infrabox/cache/ccache
 #ccache -M 2G
